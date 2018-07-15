91dc9472da945eef247577eb28434360e0fd3bad
Administrator <Administrator@192.168.6.54>
2018-07-15 20:27:17 +0800

    firs

---
+++ maven_35/src/main/java/cn/qt/maven_35/App.java
@@ -0,0 +1,13 @@
+package cn.qt.maven_35;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
